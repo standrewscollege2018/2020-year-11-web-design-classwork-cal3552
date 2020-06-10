8f745407d61742fa599abb841b5a43c8aaca9958
cal3552 <60585181+cal3552@users.noreply.github.com>
2020-06-10 12:29:59 +1200

    Create search.php

---
+++ Php/search.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="results.php" method="post">
+<input type="text" name="search" placeholder="Enter your search">
+<button type="submit" name="button">Search</button>
+    </form>
+  </body>
+</html>
