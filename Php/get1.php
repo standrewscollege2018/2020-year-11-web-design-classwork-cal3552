7479c0e09fb1c420cc13da2e85eaabb74d9a69f7
cal3552 <60585181+cal3552@users.noreply.github.com>
2020-06-10 12:29:31 +1200

    Create get1.php

---
+++ Php/get1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <a href="get2.php?name=Me&age=15">Click on this link</a>
+  </body>
+</html>
